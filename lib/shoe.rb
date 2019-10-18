fdd211e7d926f34dde7ca7341c9cf9752acca888
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-18 07:27:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,11 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
-    @brand = brand
+    self.brand = brand
   end
   
   def cobble
     self.condition = "new"
+    puts "Your shoe is"
   end
 end
